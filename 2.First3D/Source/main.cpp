80d8ea99df9316fae6b894d5bc9fb1f8e5dc3467
yujialu <yujialu@tencent.com>
2022-11-30 01:51:59 +0800

    4.6 渲染一个对象的多个副本——实例化

--- 2.First3D/Source/main.cpp
+++ 2.First3D/Source/main.cpp
@@ -31,9 +31,9 @@ GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // 分配在 display() 函数中使用的变量空间，这样它们就不必在渲染过程中分配
-GLuint mvLoc, projLoc;
+GLuint mLoc, vLoc, mvLoc, projLoc, tfLoc;
 int width, height;
-float aspect, tf;
+float aspect, timeFactor;
 glm::mat4 pMat, vMat, mMat, mvMat, tMat, rMat;
 
 void setupVertices(void) {    // 36个顶点，12个三角形，组成了放置在原点处的2×2×2立方体
@@ -63,7 +63,7 @@ void init(GLFWwindow* window) {
     const char* vp = "Source/vertShader.glsl";
     const char* fp = "Source/fragShader.glsl";
     renderingProgram = Utils::createShaderProgram(vp, fp);
-    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 420.0f;
     cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f; // 沿Y轴下移以展示透视
     setupVertices();
 }
@@ -74,46 +74,43 @@ void display(GLFWwindow* window, double currentTime) {
     glUseProgram(renderingProgram);
 
     // 获取MV矩阵和投影矩阵的统一变量
-    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    mLoc = glGetUniformLocation(renderingProgram, "m_matrix");
+    vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
     projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+    tfLoc = glGetUniformLocation(renderingProgram, "tf");          // （着色器也需要它）
+
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
 
     // 构建透视矩阵
     glfwGetFramebufferSize(window, &width, &height);
     aspect = (float)width / (float)height;
     pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
 
-    // 构建视图矩阵、模型矩阵和视图-模型矩阵
-    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
-    //mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+    timeFactor = ((float)currentTime);                             // 为了获得时间因子信息
 
-    for (int i = 0; i < 24; i++)
-    {
-        tf = currentTime + i;    // tf == "time factor（时间因子）"，声明为浮点类型
-        tMat = glm::translate(glm::mat4(1.0f), glm::vec3(sin(.35f * tf) * 8.0f, cos(.52f * tf) * 8.0f, sin(.70f * tf) * 8.0f));
-        rMat = glm::rotate(glm::mat4(1.0f), 1.75f * tf, glm::vec3(0.0f, 1.0f, 0.0f));
-        rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(1.0f, 0.0f, 0.0f));
-        rMat = glm::rotate(rMat, 1.75f * tf, glm::vec3(0.0f, 0.0f, 1.0f));
-        mMat = tMat * rMat;
-        mvMat = vMat * mMat;
+    // 将透视矩阵和MV矩阵复制给相应的统一变量
+    glUniformMatrix4fv(mLoc, 1, GL_FALSE, glm::value_ptr(mMat));
+    glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+    glUniform1f(tfLoc, (float)timeFactor);
 
-        glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
-        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+    // 将VBO关联给顶点着色器中相应的顶点属性
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-        glEnableVertexAttribArray(0);
-
-        glEnable(GL_DEPTH_TEST);
-        glDepthFunc(GL_LEQUAL);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-    }
+    // 调整OpenGL设置，绘制模型
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 1000000);
 }
 
 int main(void) {                            // main()和之前的没有变化
     if (!glfwInit()) { exit(EXIT_FAILURE); }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program 1", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Firet3D", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
     glfwSwapInterval(1);
