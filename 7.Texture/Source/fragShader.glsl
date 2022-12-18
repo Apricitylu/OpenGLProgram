dde7df840027e94efacc8503a9086e9e12334307
yujialu <yujialu@tencent.com>
2022-12-18 21:36:34 +0800

    5.7 纹理贴图：示例程序

---
+++ 7.Texture/Source/fragShader.glsl
@@ -0,0 +1,16 @@
+#version 430
+
+in vec2 tc;      // 输入插值过的材质坐标
+
+out vec4 color;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+
+layout (binding=0) uniform sampler2D samp;
+
+
+void main(void)
+{ 
+	color = texture(samp, tc);
+}
--- 7.Texture/Source/main.cpp
+++ 7.Texture/Source/main.cpp
@@ -1,55 +1,144 @@
+#pragma once
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+//区分Debug模式和Release模式
+#ifdef _DEBUG
+#pragma comment(lib,"../../Debug/Common.lib")
+#else
+#pragma comment(lib,"../../Release/Common.lib")
+#endif
+
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <cmath>
+#include <glm\glm.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <stack>
+
+#include "../../Common/utils.h"
 
 using namespace std;
 
-void init(GLFWwindow* window) {}
+#define numVAOs 1
+#define numVBOs 2
 
-void display(GLFWwindow* window, double currentTime)
-{
-	glClearColor(1.0, 0.0, 0.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+float pyramidLocX, pyramidLocY, pyramidLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+GLuint brickTexture;
+
+// 分配在 display() 函数中使用的变量空间，这样它们就不必在渲染过程中分配
+GLuint mLoc, vLoc, mvLoc, projLoc, tfLoc;
+int width, height;
+float aspect, timeFactor;
+glm::mat4 pMat, vMat, mMat, mvMat, tMat, rMat;
+stack<glm::mat4> mvStack;
+
+void setupVertices(void) {
+    float pyramidPositions[54] = {
+        -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+        -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f
+    };
+
+    float pyrTexCoords[36] = { 
+        0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,   // 前侧面
+        0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,   // 右侧面
+        0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,   // 后侧面
+        0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,   // 左侧面
+        0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,   // 底面的三角形1
+        1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f    // 底面的三角形2
+    };
+
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(vao[0]);
+    glGenBuffers(numVBOs, vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(pyramidPositions), pyramidPositions, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(pyrTexCoords), pyrTexCoords, GL_STATIC_DRAW);
 }
 
-int main(int argc, char** argv)
-{
-	//初始化GLFW库
-	if (!glfwInit())
-	{
-		exit(EXIT_FAILURE);
-	}
+void init(GLFWwindow* window) {
+    const char* vp = "Source/vertShader.glsl";
+    const char* fp = "Source/fragShader.glsl";
+    renderingProgram = Utils::createShaderProgram(vp, fp);
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+    pyramidLocX = 2.0f; pyramidLocY = 2.0f; pyramidLocZ = 0.0f; // 沿Y轴下移以展示透视
 
-	//指定了机器必须与OpenGL版本4.3兼容
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    brickTexture = Utils::loadTexture("brick1.jpg");
 
-	GLFWwindow* window = glfwCreateWindow(600, 600, "HelloWorld", NULL, NULL);//实例化GLFWwindow
-	glfwMakeContextCurrent(window);//将window与当前OpenGL上下文关联起来
+    setupVertices();
+}
 
-	//初始化GLEW库
-	if (glewInit() != GLEW_OK)
-	{
-		exit(EXIT_FAILURE);
-	}
+void display(GLFWwindow* window, double currentTime) {
+    glClear(GL_DEPTH_BUFFER_BIT);//清除深度缓冲区
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
 
-	//开启垂直同步（Vsync）
-	glfwSwapInterval(1);
+    // 获取MV矩阵和投影矩阵的统一变量
+    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
-	//调用一次init()函数
-	init(window);
+    // 构建透视矩阵
+    glfwGetFramebufferSize(window, &width, &height);
+    aspect = (float)width / (float)height;
+    pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-	while (!glfwWindowShouldClose(window))
-	{
-		display(window, glfwGetTime());	//重复调用display()函数
-		glfwSwapBuffers(window);//绘制屏幕
-		glfwPollEvents();//处理窗口相关事件（如按键事件）
-	}
+    // 将视图矩阵推入堆栈
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+    mMat *= glm::rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.3f, 1.0f, 0.0f));
+    mvMat = vMat * mMat;
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
 
-	return 0;
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, brickTexture);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glDrawArrays(GL_TRIANGLES, 0, 18);
+}
+
+int main(void) {                            // main()和之前的没有变化
+    if (!glfwInit()) { exit(EXIT_FAILURE); }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Firet3D", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+    glfwSwapInterval(1);
+
+    init(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
