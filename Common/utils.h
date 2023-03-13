f37c914c0d26f7f630bacf25b57dfced2905d111
yujialu <yujialu@tencent.com>
2023-03-14 00:27:17 +0800

    7.3 材质

--- Common/utils.h
+++ Common/utils.h
@@ -18,4 +18,22 @@ public:
 	static string readShaderSource(const char* filePath);
 	static GLuint createShaderProgram(const char* vp, const char* fp);
 	static GLuint loadTexture(const char* texImagePath);
+
+    // 黄金材质 — 环境光、漫反射、镜面反射和光泽
+    float* goldAmbient();
+    float* goldDiffuse();
+    float* goldSpecular();
+    float goldShininess();
+
+    // 白银材质 — 环境光、漫反射、镜面反射和光泽
+    float* silverAmbient();
+    float* silverDiffuse();
+    float* silverSpecular();
+    float silverShininess();
+
+    // 青铜材质 — 环境光、漫反射、镜面反射和光泽
+    float* bronzeAmbient();
+    float* bronzeDiffuse();
+    float* bronzeSpecular();
+    float bronzeShininess();
 };
