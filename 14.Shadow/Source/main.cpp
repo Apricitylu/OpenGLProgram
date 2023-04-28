f68a412f6e3e5c7cbf139108bbd6810eab4dad22
yujialu <yujialu@tencent.com>
2023-04-28 22:46:36 +0800

    8.6 阴影贴图的伪影

--- 14.Shadow/Source/main.cpp
+++ 14.Shadow/Source/main.cpp
@@ -164,6 +164,9 @@ void setupShadowBuffers(GLFWwindow* window) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+    //处理重复阴影
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
 void init(GLFWwindow* window) {
