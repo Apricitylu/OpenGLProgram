04cc4c826f10982b9cb2792d1444e7eef3f49251
Apricitylu <1376961154@qq.com>
2024-01-06 17:47:02 +0800

    14.9 噪声应用——特殊效果

---
+++ 36.Noise_Discard/Source/main.cpp
@@ -0,0 +1,337 @@
+#pragma once
+
+//区分Debug模式和Release模式
+#ifdef _DEBUG
+#pragma comment(lib,"../../Debug/Common.lib")
+#else
+#pragma comment(lib,"../../Release/Common.lib")
+#endif
+
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <glm\glm.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <vector>
+
+#include "../../Common/utils.h"
+#include "../../Common/Torus.h"
+
+
+using namespace std;
+
+#define numVAOs 1
+#define numVBOs 4
+
+float cameraX, cameraY, cameraZ;
+float LocX, LocY, LocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+// 为display() 函数分配变量
+GLuint mvLoc, projLoc, nLoc, tLoc;
+
+int width, height;
+float aspect;
+
+// 着色器统一变量中的位置
+GLuint globalAmbLoc, ambLoc, diffLoc, specLoc, posLoc, mAmbLoc, mDiffLoc, mSpecLoc, mShiLoc;
+
+glm::mat4 pMat, vMat, mMat, mvMat, invTrMat;
+glm::vec3 currentLightPos, lightPosV;   // 在模型和视觉空间中的光照位置，Vector3f类型
+float lightPos[3];                      // 光照位置的浮点数组
+
+// 初始化光照位置
+glm::vec3 initialLightLoc = glm::vec3(5.0f, 2.0f, 2.0f);
+
+// 白光特性
+float globalAmbient[4] = { 0.7f, 0.7f, 0.7f, 1.0f };
+float lightAmbient[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+float lightDiffuse[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+float lightSpecular[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+
+// 黄金材质特性
+float* matAmb = Utils::goldAmbient();
+float* matDif = Utils::goldDiffuse();
+float* matSpe = Utils::goldSpecular();
+float matShi = Utils::goldShininess();
+
+const int noiseWidth = 256;
+const int noiseHeight = 256;
+const int noiseDepth = 256;
+double noise[noiseWidth][noiseHeight][noiseDepth];
+int stripeTexture;
+
+glm::mat4 texRot;
+
+Torus myTorus(0.5f, 0.2f, 64);
+
+void setupVertices(void) {
+    std::vector<int> ind = myTorus.getIndices();
+    std::vector<glm::vec3> vert = myTorus.getVertices();
+    std::vector<glm::vec2> tex = myTorus.getTexCoords();
+    std::vector<glm::vec3> norm = myTorus.getNormals();
+
+    std::vector<float> pvalues;
+    std::vector<float> tvalues;
+    std::vector<float> nvalues;
+
+    int numVertices = myTorus.getNumVertices();
+    for (int i = 0; i < numVertices; i++) {
+        pvalues.push_back(vert[i].x);
+        pvalues.push_back(vert[i].y);
+        pvalues.push_back(vert[i].z);
+
+        tvalues.push_back(tex[i].s);
+        tvalues.push_back(tex[i].t);
+
+        nvalues.push_back(norm[i].x);
+        nvalues.push_back(norm[i].y);
+        nvalues.push_back(norm[i].z);
+    }
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(vao[0]);
+    glGenBuffers(4, vbo);           // 像以前一样生成VBO，并新增一个用于索引
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);          // 顶点位置
+    glBufferData(GL_ARRAY_BUFFER, pvalues.size() * 4, &pvalues[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);          // 纹理坐标
+    glBufferData(GL_ARRAY_BUFFER, tvalues.size() * 4, &tvalues[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);          // 法向量
+    glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);  // 索引
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ind.size() * 4, &ind[0], GL_STATIC_DRAW);
+}
+
+/*
+* 通过计算相应原始“块状”噪声图中纹素周围的8个灰度值的加权平均值来计算给定噪声图的平滑版本中的每个纹素的灰度值。
+*/
+double smoothNoise(double x1, double y1, double z1)
+{
+    // x1、y1和z1的小数部分（对于当前纹素，从当前块到下一个块的百分比）
+    double fractX = x1 - (int)x1;
+    double fractY = y1 - (int)y1;
+    double fractZ = z1 - (int)z1;
+
+    // 在X、Y和Z方向上的相邻像素的索引
+    int x2 = ((int)x1 + noiseWidth + 1) % noiseWidth;
+    int y2 = ((int)y1 + noiseHeight + 1) % noiseHeight;
+    int z2 = ((int)z1 + noiseDepth + 1) % noiseDepth;
+
+    // 通过按照3个轴方向插值灰度，平滑噪声
+    double value = 0.0;
+    value += (1 - fractX) * (1 - fractY) * (1 - fractZ) * noise[(int)x1][(int)y1][(int)z1];
+    value += (1 - fractX) * fractY * (1 - fractZ) * noise[(int)x1][(int)y2][(int)z1];
+    value += fractX * (1 - fractY) * (1 - fractZ) * noise[(int)x2][(int)y1][(int)z1];
+    value += fractX * fractY * (1 - fractZ) * noise[(int)x2][(int)y2][(int)z1];
+
+    value += (1 - fractX) * (1 - fractY) * fractZ * noise[(int)x1][(int)y1][(int)z2];
+    value += (1 - fractX) * fractY * fractZ * noise[(int)x1][(int)y2][(int)z2];
+    value += fractX * (1 - fractY) * fractZ * noise[(int)x2][(int)y1][(int)z2];
+    value += fractX * fractY * fractZ * noise[(int)x2][(int)y2][(int)z2];
+    return value;
+}
+
+double turbulence(double x, double y, double z, double maxZoom)
+{
+    double sum = 0.0, zoom = maxZoom;
+    while (zoom >= 1.0) {                  // 最后一遍是当zoom = 1时
+       // 计算平滑后的噪声图的加权和
+        sum = sum + smoothNoise(x / zoom, y / zoom, z / zoom) * zoom;
+        zoom = zoom / 2.0;                  // 对每个2的幂的缩放因子
+    }
+    sum = 128.0 * sum / maxZoom;           // 对不大于64的maxZoom值，保证RGB < 256
+    return sum;
+}
+
+// 按照由generate3Dpattern()构建的图案，用蓝色、黄色的RGB值来填充字节数组
+void fillDataArray(GLubyte data[])
+{
+    double xyPeriod = 40.0;//年轮的数量
+    double turbPower = 0.15;//调整条纹中的扰动量（将其设置为0，使条纹不受干扰）
+    double turbSize = 32.0;//调整生成湍流时使用的缩放系数
+    for (int i = 0; i < noiseWidth; i++) {
+        for (int j = 0; j < noiseHeight; j++) {
+            for (int k = 0; k < noiseDepth; k++) {
+                //使用三角函数指定与Z轴等距的X和Y值
+                double xValue = (i - (double)noiseWidth / 2.0) / (double)noiseWidth;
+                double yValue = (j - (double)noiseHeight / 2.0) / (double)noiseHeight;
+                //double distanceFromZ = sqrt(xValue * xValue + yValue * yValue);
+                double distanceFromZ = sqrt(xValue * xValue + yValue * yValue) + turbPower * turbulence(i, j, k, turbSize) / 256.0;
+                double sineValue = 128.0 * abs(sin(2.0 * xyPeriod * distanceFromZ * 3.14159));//正弦波循环重复此过程
+
+                //根据此正弦波均匀地升高和降低红色和绿色成分，以产生不同的棕色调。
+                //变量sineValue保持精确的色调，可以通过稍微偏移一个分量或另一个分量来调整（在这种情况下，将红色增加80，将绿色增加30）。
+                float redPortion = (float)(80 + (int)sineValue);
+                float greenPortion = (float)(30 + (int)sineValue);
+                float bluePortion = 0.0f;
+
+                data[i * (noiseWidth * noiseHeight * 4) + j * (noiseHeight * 4) + k * 4 + 0] = (GLubyte)redPortion;
+                data[i * (noiseWidth * noiseHeight * 4) + j * (noiseHeight * 4) + k * 4 + 1] = (GLubyte)greenPortion;
+                data[i * (noiseWidth * noiseHeight * 4) + j * (noiseHeight * 4) + k * 4 + 2] = (GLubyte)bluePortion;
+                data[i * (noiseWidth * noiseHeight * 4) + j * (noiseHeight * 4) + k * 4 + 3] = (GLubyte)255;
+            }
+        }
+    }
+}
+// 构建条纹的3D图案
+void generateNoise()
+{
+    int xStep = 0, yStep = 0, zStep = 0, sumSteps = 0;
+    for (int x = 0; x < noiseWidth; x++) {
+        for (int y = 0; y < noiseHeight; y++) {
+            for (int z = 0; z < noiseDepth; z++) {
+                noise[x][y][z] = (double)rand() / (RAND_MAX + 1.0);
+            }
+        }
+    }
+}
+
+// 将顺序字节数据数组加载进纹理对象, 其方式类似于前面5.12
+int load3DTexture() {
+    GLuint textureID;
+    GLubyte* data = new GLubyte[noiseWidth * noiseHeight * noiseDepth * 4];
+
+    fillDataArray(data);//图像数据被格式化为对应于RGBA颜色分量的字节序列
+
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_3D, textureID);
+    glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexStorage3D(GL_TEXTURE_3D, 1, GL_RGBA8, noiseWidth, noiseHeight, noiseDepth);
+    glTexSubImage3D(GL_TEXTURE_3D, 0, 0, 0, 0, noiseWidth, noiseHeight, noiseDepth, GL_RGBA, GL_UNSIGNED_INT_8_8_8_8_REV, data);
+    return textureID;
+}
+
+void init(GLFWwindow* window) {
+    const char* vp = "Source/vertShader.glsl";
+    const char* fp = "Source/fragShader.glsl";
+    renderingProgram = Utils::createShaderProgram(vp, fp);
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 2.0f;
+    LocX = 0.0f; LocY = 0.0f; LocZ = 0.0f; // 沿Y轴下移以展示透视
+    setupVertices();
+
+    generateNoise();                   // 3D图案和纹理只加载一次，所以在init()里作
+    stripeTexture = load3DTexture();       // 为3D纹理保存整型图案ID
+
+    // 旋转应用于纹理坐标——增加额外的木纹变化
+    texRot = glm::rotate(glm::mat4(1.0f), Utils::toRadians(20.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+void installLights(glm::mat4 vMatrix) {
+    // 将光源位置转换为视图空间坐标，并存入浮点数组
+    lightPosV = glm::vec3(vMatrix * glm::vec4(currentLightPos, 1.0));
+    lightPos[0] = lightPosV.x;
+    lightPos[1] = lightPosV.y;
+    lightPos[2] = lightPosV.z;
+
+    // 在着色器中获取光源位置和材质属性
+    globalAmbLoc = glGetUniformLocation(renderingProgram, "globalAmbient");
+    ambLoc = glGetUniformLocation(renderingProgram, "light.ambient");
+    diffLoc = glGetUniformLocation(renderingProgram, "light.diffuse");
+    specLoc = glGetUniformLocation(renderingProgram, "light.specular");
+    posLoc = glGetUniformLocation(renderingProgram, "light.position");
+    mAmbLoc = glGetUniformLocation(renderingProgram, "material.ambient");
+    mDiffLoc = glGetUniformLocation(renderingProgram, "material.diffuse");
+    mSpecLoc = glGetUniformLocation(renderingProgram, "material.specular");
+    mShiLoc = glGetUniformLocation(renderingProgram, "material.shininess");
+
+    // 在着色器中为光源与材质统一变量赋值
+    glProgramUniform4fv(renderingProgram, globalAmbLoc, 1, globalAmbient);
+    glProgramUniform4fv(renderingProgram, ambLoc, 1, lightAmbient);
+    glProgramUniform4fv(renderingProgram, diffLoc, 1, lightDiffuse);
+    glProgramUniform4fv(renderingProgram, specLoc, 1, lightSpecular);
+    glProgramUniform3fv(renderingProgram, posLoc, 1, lightPos);
+    glProgramUniform4fv(renderingProgram, mAmbLoc, 1, matAmb);
+    glProgramUniform4fv(renderingProgram, mDiffLoc, 1, matDif);
+    glProgramUniform4fv(renderingProgram, mSpecLoc, 1, matSpe);
+    glProgramUniform1f(renderingProgram, mShiLoc, matShi);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+    glClear(GL_DEPTH_BUFFER_BIT);//清除深度缓冲区
+    glClear(GL_COLOR_BUFFER_BIT);
+    glUseProgram(renderingProgram);
+
+    // 用于模型-视图变换、投影以及逆转置(法向量)矩阵的统一变量
+    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+    nLoc = glGetUniformLocation(renderingProgram, "norm_matrix");
+
+    tLoc = glGetUniformLocation(renderingProgram, "texRot");
+    glUniformMatrix4fv(tLoc, 1, false, glm::value_ptr(texRot));
+
+    // 构建透视矩阵
+    glfwGetFramebufferSize(window, &width, &height);
+    aspect = (float)width / (float)height;
+    pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 radians = 60 degrees
+
+    // 构建视图矩阵、模型矩阵和视图-模型矩阵
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+    mMat = glm::translate(glm::mat4(1.0f), glm::vec3(LocX, LocY, LocZ));
+    mMat *= glm::rotate(glm::mat4(1.0f), (float)currentTime * 0.1f, glm::vec3(1.0, 0.0, 0.0));
+
+    // 通过合并矩阵v和m，创建模型-视图(MV)矩阵，如前
+    mvMat = vMat * mMat;
+
+    // 基于当前光源位置，初始化光照
+    currentLightPos = glm::vec3(initialLightLoc.x, initialLightLoc.y, initialLightLoc.z);
+    installLights(vMat);
+
+    // 构建MV矩阵的逆转置矩阵，以变换法向量
+    invTrMat = glm::transpose(glm::inverse(mvMat));
+
+    // 将MV、PROJ以及逆转置(法向量)矩阵传入相应的统一变量
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+    glUniformMatrix4fv(nLoc, 1, GL_FALSE, glm::value_ptr(invTrMat));
+
+    // 在顶点着色器中，将顶点缓冲区(VBO #0)绑定到顶点属性#0
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    // 在顶点着色器中，将法向缓冲区(VBO #2)绑定到顶点属性#1
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+    glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, 0);
+    glEnableVertexAttribArray(1);
+
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CCW);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_3D, stripeTexture);//指定了纹理类型GL_TEXTURE_3D
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[3]);
+    glDrawElements(GL_TRIANGLES, myTorus.getNumIndices(), GL_UNSIGNED_INT, 0);
+}
+
+int main(void) {                            // main()和之前的没有变化
+    if (!glfwInit()) { exit(EXIT_FAILURE); }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Demo", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+    glfwSwapInterval(1);
+
+    init(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
---
+++ 36.Noise_Discard/Source/vertShader.glsl
@@ -0,0 +1,47 @@
+#version 430
+layout (location=0) in vec3 vertPos;
+layout (location=1) in vec3 vertNormal;
+out vec3 varyingNormal;      // 视觉空间顶点法向量
+out vec3 varyingLightDir;    // 指向光源的向量
+out vec3 varyingVertPos;     // 视觉空间中的顶点位置
+out vec3 varyingHalfVector;  // 角平分线向量 H 作为新增的输出
+
+out vec3 originalPosition;   // 原始模型顶点将被用于纹理坐标
+
+struct PositionalLight
+{ 
+    vec4 ambient; 
+    vec4 diffuse; 
+    vec4 specular; 
+    vec3 position;
+};
+
+struct Material
+{ 
+    vec4 ambient; 
+    vec4 diffuse; 
+    vec4 specular; 
+    float shininess;
+};
+
+uniform vec4 globalAmbient;
+uniform PositionalLight light;
+uniform Material material;
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+uniform mat4 norm_matrix;   // 用来变换法向量
+uniform mat4 texRot;
+
+void main(void)
+{ 
+    // 输出顶点位置、光照方向和法向量到光栅器以进行插值
+    varyingVertPos = (mv_matrix * vec4(vertPos,1.0)).xyz; 
+    varyingLightDir = light.position - varyingVertPos; 
+    varyingNormal = (norm_matrix * vec4(vertNormal,1.0)).xyz; 
+    varyingHalfVector = (varyingLightDir + (-varyingVertPos)).xyz; 
+
+    //originalPosition = vertPos;          // 将原始模型坐标传递，用作3D纹理坐标
+    originalPosition = vec3(texRot * vec4(vertPos,1.0)).xyz;
+
+    gl_Position = proj_matrix * mv_matrix * vec4(vertPos,1.0);
+}
